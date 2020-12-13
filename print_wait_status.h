09591e0be1ee3b74df431fb966702d9c250df13a
4thel00z <4thel00z@gmail.com>
2020-12-13 23:29:40 +0100

    [master] 💥 Init all the things! 💥
    Signed-off-by: 4thel00z <4thel00z@gmail.com>

---
+++ print_wait_status.h
@@ -0,0 +1,10 @@
+/* print_wait_status.h
+
+   Header file for print_wait_status.c.
+*/
+#ifndef PRINT_WAIT_STATUS_H     /* Prevent accidental double inclusion */
+#define PRINT_WAIT_STATUS_H
+
+void printWaitStatus(const char *msg, int status);
+
+#endif
